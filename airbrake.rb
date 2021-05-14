ff426082e5ffc790a7d3ef591797fa5e8603f8a4
airbrake-ci <support@airbrake.io>
2021-05-13 19:21:37 -0500

    Brew formula update for airbrake version v0.12.0

--- airbrake.rb
+++ airbrake.rb
@@ -5,20 +5,20 @@
 class Airbrake < Formula
   desc "Airbrake CLI"
   homepage "https://airbrake.io"
-  version "0.10.0"
+  version "0.12.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/airbrake/airbrake-cli/releases/download/v0.10.0/airbrake_0.10.0_mac-os_x86_64.tar.gz"
-    sha256 "9ce42d8860e7ec34b655b98ef56907804815ea757d12df182f64ce9e8db1b77d"
+    url "https://github.com/airbrake/airbrake-cli/releases/download/v0.12.0/airbrake_0.12.0_mac-os_x86_64.tar.gz"
+    sha256 "15dc94020948e66c4a1971451530862b0fc6c8fb5cb66775f635f37233bc0950"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/airbrake/airbrake-cli/releases/download/v0.10.0/airbrake_0.10.0_mac-os_arm64.tar.gz"
-    sha256 "a025f36dd0633f8e8b850702b92ecd3dcccaaf341c2fe5c741fb15042aad02c3"
+    url "https://github.com/airbrake/airbrake-cli/releases/download/v0.12.0/airbrake_0.12.0_mac-os_arm64.tar.gz"
+    sha256 "d73584832f2bbe5e23cc8311307f8e8974872088ca8b8d661e00dafd79739263"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/airbrake/airbrake-cli/releases/download/v0.10.0/airbrake_0.10.0_linux_x86_64.tar.gz"
-    sha256 "7e22dd0c0958fff8a7f2725358480ee4952e512e1814b999d53dd1b010049be6"
+    url "https://github.com/airbrake/airbrake-cli/releases/download/v0.12.0/airbrake_0.12.0_linux_x86_64.tar.gz"
+    sha256 "d71c5b0b2454da1eadac580e9755aa75b1b056095cda96603de48d931f07ccb4"
   end
 
   def install
